e11d5d13e777a7e6d3720fe7fe12233dfbf81400
NewSkyLineTwitter <69757762+NewSkyLineTwitter@users.noreply.github.com>
2021-12-07 21:09:11 +0100

    finished unit 7

--- Programmieren und Software Engineering/POS1/Übungen/07_unit7/geom.py
+++ Programmieren und Software Engineering/POS1/Übungen/07_unit7/geom.py
@@ -4,12 +4,11 @@ file_name: geom.py
 """
 
 from geomlib import *
-import getpass
 from math import sqrt
 
 
 print("Willkommen beim geometrischen Wunderzwerg!\n")
-eingabe = getpass.getpass("(1) Berchnung der Entfernung: eindimensional\n(2) Berchnung der Entfernung: zweidimensional\n(3) Berchnung der Entfernung: dreidimensional\n")
+eingabe = input("(1) Berchnung der Entfernung: eindimensional\n(2) Berchnung der Entfernung: zweidimensional\n(3) Berchnung der Entfernung: dreidimensional\n(4) Zechenen eines Vielecks\n(5) Dreieck Zeichenen mit min. 3 oder max. x-y-Werten:\n(6) Stern aus mal Zeichen")
 
 
 def eindimensional():
@@ -48,11 +47,49 @@ def dreidimensional():
     print(distance3(float(x1), float(y1), float(z1), float(x2), float(y2), float(z2)))
 
 
+def reg_polygon():
+    x = float(input("Geben Sie den start x-Wert ein: "))
+    y = float(input("Geben Sie den start y-Wert ein: "))
+    laenge = float(input("Geben Sie die Seitnlänge ein: "))
+    ecken = int(input("Geben Sie die Anzahl von den Ecken ein:"))
+    regular_polygon(x, y, laenge, ecken)
+    
+
+def polygon2():
+    x1 = input("Geben Sie den x1 Wert ein: ")
+    y1 = input("Geben Sie den y1 Wert ein: ")
+    x2 = input("Geben Sie den x2 Wert ein: ")
+    y2 = input("Geben Sie den y2 Wert ein: ")
+    x3 = input("Geben Sie den x3 Wert ein: ")
+    y3 = input("Geben Sie den y3 Wert ein: ")
+    x4 = input("Geben Sie den x4 Wert ein: ")
+    y4 = input("Geben Sie den y4 Wert ein: ")
+    x5 = input("Geben Sie den x5 Wert ein: ")
+    y5 = input("Geben Sie den y5 Wert ein: ")
+    if x4 == '' and y4 == '':
+        print(polygon(float(x1), float(y1), float(x2), float(y2), float(x3), float(y3)))
+    elif x5 == '' and y5 == '' and x4 is not '' and y4 is not '':
+        print(polygon(float(x1), float(y1), float(x2), float(y2), float(x3), float(y3), float(x4), float(y4)))
+    elif x4 is not '' and y4 is not '' and x5 is not '' and y5 is not '':
+        print(polygon(float(x1), float(y1), float(x2), float(y2), float(x3), float(y3), float(x4), float(y4), float(x5), float(y5)))
+
+
+def stars_triangle(number):
+    for i in range(1, number + 1):
+        print(i * "*")
+
+
 if eingabe == "1":
     eindimensional()
 elif eingabe == "2":
     zweidimensional()
 elif eingabe == "3":
     dreidimensional()
+elif eingabe == "4":
+    reg_polygon()
+elif eingabe == "5":
+    polygon2()
+elif eingabe == "6":
+    stars_triangle(int(input("Geben Sie eine Zahl ein: ")))
 else:
     print("Falsche Eingabe!")
