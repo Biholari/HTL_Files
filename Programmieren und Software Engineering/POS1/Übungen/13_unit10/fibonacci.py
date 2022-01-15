a64dd012488c2639a9ce70e249cd459bbfbbab05
NewSkyLineTwitter <69757762+NewSkyLineTwitter@users.noreply.github.com>
2022-01-15 10:21:17 +0100

    Add files via upload

---
+++ Programmieren und Software Engineering/POS1/Übungen/13_unit10/fibonacci.py
@@ -0,0 +1,9 @@
+def getFibNth(n):
+    if n == 1:
+        return 0
+    if n <= 3:
+        return 1
+    
+    return getFibNth(n - 1) + getFibNth(n - 2)
+
+print(getFibNth(4))
---
+++ Programmieren und Software Engineering/POS1/Übungen/13_unit10/name_spaces1.py
@@ -0,0 +1,9 @@
+"""
+author: Oppermann Fabian
+file_name: name_spaces1.py
+"""
+
+name = input("Gib einen Namen ein: ")
+
+for i in range(len(name)):
+    print(name[i], end=" ")
