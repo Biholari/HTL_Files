f17a508fd2c614b58adb93c5a9588d343ff00f00
NewSkyLineTwitter <69757762+NewSkyLineTwitter@users.noreply.github.com>
2022-01-24 19:52:06 +0100

    changed units

--- Programmieren und Software Engineering/POS1/Übungen/11_unit8/regular_polygon.py
+++ Programmieren und Software Engineering/POS1/Übungen/11_unit8/regular_polygon.py
@@ -1,6 +1,6 @@
 """
 author: Oppermann Fabian
-file_name regular_polygon.py
+file_name regular_polygon2.py
 """
 
 import turtle as tr
@@ -11,13 +11,16 @@ sides = int(input("Anzahl der Seiten: "))
 
 def regular_polygon2():
     colT = []
+    tr.colormode(255)
     for i in range(sides):
-        colN = input(f"Farbe {i+1}: ")
-        colT.append(colN)
-    for j in colT:
-        tr.pencolor(j)
+        red = int(input("Rodanteil: "))
+        green = int(input("Grünanteil: "))
+        blue = int(input("Blauanteil: "))
+        colT.append((red, green, blue))
+    for i in colT:
+        tr.pencolor(i)
         tr.fd(length)
         tr.lt(360 / sides)
 
-    
+
 regular_polygon2()
