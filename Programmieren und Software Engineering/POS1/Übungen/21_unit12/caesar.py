93ffe5ac83b9bd8f8ec17399d58af143b65616db
Fabian Oppermann <69757762+NewSkyLineTwitter@users.noreply.github.com>
2022-05-10 15:04:43 +0200

    Update caesar.py
    Added functionality for Unicode

--- Programmieren und Software Engineering/POS1/Übungen/21_unit12/caesar.py
+++ Programmieren und Software Engineering/POS1/Übungen/21_unit12/caesar.py
@@ -4,44 +4,84 @@ file_name: caeser.py
 """
 
 msg = input("Satz oder Wort: ")
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-caeser = "DEFGHIJKLMNOPQRSTUVWXYZABC"
-alphabet_lower = "abcdefghijklmnopqrstuvwxyz"
-caeser_lower = "defghijklmnopqrstuvwxyzabc"
+
+"""
+# Erste Version mit Dictionarys!
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz "
+caeser = "DEFGHIJKLMNOPQRSTUVWXYZABCdefghijklmnopqrstuvwxyzabc "
 caDict = {}
-caDict_lower = {}
-wordsList = []
-decryptWordList = []
-# key is alphabet | value is caeser
+
+
+for k1, k2 in zip(alphabet, caeser):
+    caDict[k1] = k2
 
 
 def encrypt1(msg):
-    for key, key2 in zip(alphabet, caeser):
-        caDict[key] = key2
-    for lkey, lkey2 in zip(alphabet_lower, caeser_lower):
-        caDict_lower[lkey] = lkey2
+    Nachricht = ""
     for word in msg:
-        if word == " ":
-            wordsList.append(" ")
-        else:
-            if word.islower():
-                wordsList.append(caDict_lower[word])
-            else:
-                wordsList.append(caDict[word])
+        Nachricht += caDict[word]
+    return Nachricht
 
 
+def Invert(Dict):
+    InvDic = {}
+    for k, v in Dict.items():
+        InvDic[v] = k
+    return InvDic
+        
+        
 def encrypt2():
-    for word in wordsList:
-        if word.islower():
-            for k, v in caDict_lower.items():
-                if v == word:
-                    decryptWordList.append(k)
-        else:
-            for k, v in caDict.items():
-                if v == word:
-                    decryptWordList.append(k)
-    return "".join(decryptWordList)
+    Nachricht = ""
+    for word in encrypt1(msg):
+        Nachricht += Invert(caDict)[word]
+    return Nachricht
 
 
 print(encrypt1(msg))
 print(encrypt2())
+"""
+
+
+# Zweite Version mit Ascii Tabelle
+def encrypt2(msg, k):
+    Nachricht = ""
+    for word in msg:
+        if word.isupper():
+            if ord(word) + 3 >= 91:
+                Nachricht += chr((ord(word) + k) - 26)
+            elif word == " ":
+                Nachricht += " "
+            else:
+                Nachricht += chr(ord(word) + k)
+        else:
+            if ord(word) + 3 >= 122:
+                Nachricht += chr((ord(word) + k) - 26)
+            elif word == " ":
+                Nachricht += " "
+            else:
+                Nachricht += chr(ord(word) + k)           
+    return Nachricht
+        
+
+def decrypt2(msg, k):
+    Nachricht = ""
+    for word in msg:
+        if word.isupper():
+            if ord(word) - k + 26 >= 91:
+                Nachricht += chr((ord(word) - k))
+            elif word == " ":
+                Nachricht += " "
+            else:
+                Nachricht += chr(ord(word)+ 26 - k)
+        else:
+            if ord(word) - k >= 97:
+                Nachricht += chr(ord(word) - 3)
+            elif word == " ":
+                Nachricht += " "
+            else:
+                Nachricht += chr(ord(word) - 3 + 26)
+    return Nachricht
+
+
+print(encrypt2(msg, 3))
+print(decrypt2(encrypt2(msg, 3), 3))
