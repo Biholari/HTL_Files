fd6f024374fac16af5de76bf5a897a93effebd9b
Fabian Oppermann <69757762+NewSkyLineTwitter@users.noreply.github.com>
2022-05-23 08:02:32 +0200

    Added Multiplikation table

--- Programmieren und Software Engineering/POS1/Übungen/23_repetition5/sets2.py
+++ Programmieren und Software Engineering/POS1/Übungen/23_repetition5/sets2.py
@@ -3,9 +3,22 @@ author: Oppermann Fabian
 file_name: sets.py
 """
 
+
 def cross_product(a, b):
     return {(x, y) for x in a for y in b}
+
+
 # print(cross_product({1, 2, 3}, {4, 5}))
+
+
+def output_mul_table(n):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            print(i*j, end=" ")
+        print()
+
+
+# output_mul_table(8)
 
 
 def in_set(e, a): # e = list | a = list
@@ -15,27 +28,39 @@ def in_set(e, a): # e = list | a = list
         if e[indxE] == a[indxA]:
             return True
         indxA += 1
+        
+        
 # print(in_set([5, 1, 22, 25], [1, 6, 5, -1, 10]))
 
 
 def reverse_list(lst):
     print(",".join(str(x) for x in lst[::-1]))
+    
+    
 # reverse_list([1, "a", 2.5])
 
 
 def to_str(lst):
     return str(",".join(str(x) for x in lst))
+
+
 # print(to_str([1, "a", 2.5]))
+
 
 def reverse_number(n):
     return int(str(n)[::-1])
+
+
 # print(reverse_number(123))
+
 
 def is_palindrome(n):
     if n == reverse_number(n):
         return True
     else:
         return False
+    
+    
 # print(is_palindrome(12321))
 
 
@@ -45,7 +70,10 @@ def add(lst):
         for j in i:
             res += j
     return res
+
+
 # print(add([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+
 
 def minmax(lst):
     min = lst[0][0]
@@ -57,9 +85,13 @@ def minmax(lst):
             if lst[i][j] > max:
                 max = lst[i][j]
     return (min, max)
+
+
 # print(minmax([[4, 5, 6], [7, 8, 9], [1, 2, 3]]))
 
 
 def change_case(s):
     return s.swapcase()
+
+
 # print(change_case("aABb1 _Cc"))
