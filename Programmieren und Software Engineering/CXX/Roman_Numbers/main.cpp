379c7d74af34dc312157e468d36337d67aea08fd
Fabian Oppermann <ofabi@gmx.at>
2022-12-22 10:41:55 +0100

    Code - Added normal code - Started extension

--- Programmieren und Software Engineering/CXX/Roman_Numbers/main.cpp
+++ Programmieren und Software Engineering/CXX/Roman_Numbers/main.cpp
@@ -1,17 +1,40 @@
 #include <iostream>
+#include <string>
 #include "roman_number.h"
 
 using namespace std;
 
-int main() {
+int main()
+{
+   /* string input;
+    while(true) {
+        cout << "Enter a number (either roman or decimal): ";
+        cin >> input;
+        cout << endl;
+
+        if (!cin.fail())
+            break;
+
+        cout << "Invalid input! Please try again: ";
+
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    if (isdigit(input[0])) {
+        int number = stoi(input);
+        Roman_Number roman_number(number);
+
+        cout << "You entered the number: " << number << ". Which is in roman system: " << roman_number.get_value() << endl;
+    }
+    else {
+        Roman_Number roman_number(input);
+        cout << "You entered the roman number: " << input << ". Which is in decimal: " << roman_number.get_number() << endl;
+    }*/
+
     Roman_Number roman_number(4);
 
-    try
-    {
-        cout << roman_number.get_number() << endl;
-    }
-    catch(const string& e)
-    {
-        std::cerr << e << endl;
-    }
+    std::cout << roman_number.get_number() << endl;
+
+    return 0;
 }
--- Programmieren und Software Engineering/CXX/Roman_Numbers/roman_number.cpp
+++ Programmieren und Software Engineering/CXX/Roman_Numbers/roman_number.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
+#include <string>
 #include "roman_number.h"
 
 using namespace std;
 
 Roman_Digit Roman_Number::single_digits[] = {"M", 1000, 1000, "D", 500, 100, "C", 100, 100, "L", 50, 10, "X", 10, 10, "V", 5, 1, "I", 1, 1};
 Roman_Digit Roman_Number::double_digits[] = {"CM", 900, 90, "CD", 400, 90, "XC", 90, 9, "XL", 40, 9, "IX", 9, 0, "IV", 4, 0, "", 0, 0};
-
-int Roman_Number::roman_to_dec(string roman)
-{
-}
 
 string Roman_Number::dec_to_roman(int dec_value)
 {
@@ -52,14 +49,78 @@ string Roman_Number::dec_to_roman(int dec_value)
     return result;
 }
 
+int Roman_Number::roman_to_dec(string roman)
+{
+    int decimal = 0;
+    int next_value = 1000;
+    std::string original = roman;
+    while (!roman.empty())
+    {
+        std::string digit = roman.substr(0, 2);
+        bool found = false;
+
+        for (auto const &rd : double_digits)
+        {
+            if (rd.digit == digit)
+            {
+                if (rd.value > next_value)
+                    throw std::string("Falsche römische Zahl");
+
+                decimal += rd.value;
+                roman.erase(0, 2);
+                next_value = rd.next;
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            digit = roman.substr(0, 1);
+            for (auto const &rd : single_digits)
+            {
+                if (rd.digit == digit)
+                {
+                    if (rd.value > next_value)
+                        throw std::string("Falsche römische Zahl");
+
+                    decimal += rd.value;
+                    roman.erase(0, 1);
+                    next_value = rd.next;
+                    found = true;
+                    break;
+                }
+            }
+        }
+        if (!found)
+            throw std::string("Falsche römische Zahl");
+    }
+    return decimal;
+}
+
 Roman_Number::Roman_Number(int value)
 {
-    this->number = dec_to_roman(value);
+    try
+    {
+        this->number = dec_to_roman(value);
+    }
+    catch (const string &e)
+    {
+        cerr << "Nummer kann nicht unter 0 sein" << endl;
+        exit(1);
+    }
 }
 
 Roman_Number::Roman_Number(string number)
 {
-    this->value = roman_to_dec(number);
+    try
+    {
+        this->value = roman_to_dec(number);
+    }
+    catch (const string &e)
+    {
+        cerr << "Falsche Römische Zahl" << endl;
+        exit(1);
+    }
 }
 
 string Roman_Number::get_number()
