c78d16fd31616d51e87520943e84f1b1f2a24cc6
NewSkyLine-dev <fabian.oppermann@gmx.net>
2024-09-09 22:47:55 +0200

    First C# project

---
+++ POS/C#/Multithreaded Counter/Multithreaded Counter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multithreaded_Counter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int id = 0; id < 3; id++)
+            {
+                if (counter % id == 0)
+                {
+                    Console.WriteLine("ID: {0,3} Counter: {1,8} Modulo: {2}", id, counter, counter % id);
+                }
+
+                Thread thread = new Thread();
+            }
+        }
+    }
+}
