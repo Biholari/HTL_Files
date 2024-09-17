9ba50b716333527dc1c7c4c45393a0b5e8276ee5
NewSkyLine-dev <fabian.oppermann@gmx.net>
2024-09-17 19:31:55 +0200

    Deleted Visual Studio generated files from 'Multithreadded Counter'

--- POS/C#/Multithreaded Counter/Multithreaded Counter/Program.cs
+++ POS/C#/Multithreaded Counter/Multithreaded Counter/Program.cs
@@ -1,26 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Multithreaded_Counter
 {
     internal class Program
     {
+        static int counter = 0;  // Gemeinsamer Counter
+        private static object lockObject = new object();  // Lock-Objekt zur Sicherstellung der Synchronisation
+
         static void Main(string[] args)
         {
-            for (int id = 0; id < 3; id++)
+            // Überprüfen, ob die richtigen Argumente übergeben wurden
+            if (args.Length != 2 || !int.TryParse(args[0], out int threadCount) || !int.TryParse(args[1], out int iterationsPerThread))
             {
-                if (counter % id == 0)
-                {
-                    Console.WriteLine("ID: {0,3} Counter: {1,8} Modulo: {2}", id, counter, counter % id);
-                }
+                Console.WriteLine("Bitte geben Sie die Anzahl der Threads und die Anzahl der Iterationen pro Thread als Argumente an.");
+                Console.WriteLine("Beispiel: 5 10");
+                return;
+            }
 
-                Thread thread = new Thread();
+            // Array für die Threads
+            Thread[] threads = new Thread[threadCount];
+
+            // Threads erstellen und starten
+            for (int i = 0; i < threadCount; i++)
+            {
+                int threadId = i + 2;  // Thread-ID beginnt bei 2
+                threads[i] = new Thread(() => IncrementCounter(threadId, iterationsPerThread));
+            }
+
+            // Alle Threads gleichzeitig starten
+            foreach (Thread thread in threads)
+            {
+                thread.Start();
+            }
+
+            // Warten, bis alle Threads fertig sind
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
+        }
+
+        static void IncrementCounter(int id, int iterations)
+        {
+            for (int i = 0; i < iterations; i++)
+            {
+                lock (lockObject)  // Synchronisation, um Datenkorruption zu vermeiden (kritischer Abschnitt)
+                {
+                    if (counter % id == 0)
+                    {
+                        Console.WriteLine("ID: {0,3} Counter: {1,8} Modulo: {2}", id, counter, counter % id);
+                    }
+
+                    counter++;
+                }
             }
         }
     }
+
 }
