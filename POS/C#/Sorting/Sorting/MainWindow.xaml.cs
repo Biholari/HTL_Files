5178d6f55555f0c58f537a9c105abe9016276556
NewSkyLine-dev <fabian.oppermann@gmx.net>
2024-11-15 16:47:31 +0100

    POS Sorting

---
+++ POS/C#/Sorting/Sorting/MainWindow.xaml.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Sorting
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public ObservableCollection<int> sortList = new ObservableCollection<int>();
+        private readonly Random rand = new Random();
+        private int _checks = 0;
+        private int _swaps = 0;
+        private int _selected = -1;
+
+        public ObservableCollection<int> List
+        {
+            set
+            {
+                sortList = value;
+                NotifyPropertyChanged(x => x.List);
+            }
+            get
+            {
+                return sortList;
+            }
+        }
+        public int Checks
+        {
+            set
+            {
+                _checks = value;
+                NotifyPropertyChanged(x => x.Checks);
+            }
+            get
+            {
+                return _checks;
+            }
+        }
+        public int Swaps
+        {
+            set
+            {
+                _swaps = value;
+                NotifyPropertyChanged(x => x.Swaps);
+            }
+            get
+            {
+                return _swaps;
+            }
+        }
+
+        public int Selected
+        {
+            set
+            {
+                _selected = value;
+                NotifyPropertyChanged(x => x.Selected);
+            }
+            get
+            {
+                return _selected;
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 50; i++)
+            {
+                sortList.Add(rand.Next(200));
+            }
+            Checks = 0;
+            Swaps = 0;
+            this.DataContext = this;
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            int size = sortList.Count;
+            Checks = 0;
+            Swaps = 0;
+            ThreadPool.QueueUserWorkItem(o =>
+            {
+                bool swapped = false;
+                do
+                {
+                    swapped = false;
+                    for (int i = 0; i < size - 1; ++i, Selected = i)
+                    {
+                        try
+                        {
+                            Dispatcher.Invoke(DispatcherPriority.Normal, new DispatcherOperationCallback(delegate
+                              {
+                                  Checks++;
+                                  if (sortList[i] > sortList[i + 1])
+                                  {
+                                      Swaps++;
+                                      (sortList[i + 1], sortList[i]) = (sortList[i], sortList[i + 1]);
+                                      swapped = true;
+                                  }
+                                  return null;
+                              }), null);
+                        }
+                        catch (Exception ex)
+                        {
+                            Debug.WriteLine(ex.ToString());
+                        }
+                        Thread.Sleep(50);
+                    }
+                    size = size - 1;
+                } while (swapped == true);
+
+            });
+        }
+
+        private void Reverse_Click(object sender, RoutedEventArgs e)
+        {
+            int size = sortList.Count;
+            Checks = 0;
+            Swaps = 0;
+            ThreadPool.QueueUserWorkItem(o =>
+            {
+                bool swapped = false;
+                do
+                {
+                    swapped = false;
+                    for (int i = 0; i < size - 1; ++i, Selected = i)
+                    {
+                        try
+                        {
+                            Dispatcher.Invoke(DispatcherPriority.Normal, new DispatcherOperationCallback(delegate
+                              {
+                                  Checks++;
+                                  if (sortList[i] < sortList[i + 1])
+                                  {
+                                      Swaps++;
+                                      (sortList[i + 1], sortList[i]) = (sortList[i], sortList[i + 1]);
+                                      swapped = true;
+                                  }
+                                  return null;
+                              }), null);
+                        }
+                        catch (Exception ex)
+                        {
+                            Debug.WriteLine(ex.ToString());
+                        }
+                        Thread.Sleep(50);
+                    }
+                    size = size - 1;
+                } while (swapped == true);
+
+            });
+        }
+
+        #region INotifyPropertyChanged Member
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged<TValue>(Expression<Func<MainWindow, TValue>> propertySelector)
+        {
+            if (PropertyChanged != null)
+            {
+                MemberExpression memberExpression = propertySelector.Body as System.Linq.Expressions.MemberExpression;
+                if (memberExpression != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(memberExpression.Member.Name));
+                }
+            }
+        }
+        #endregion
+    }
+}
