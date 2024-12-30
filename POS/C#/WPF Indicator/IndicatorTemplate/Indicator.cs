8675bc87c51708a5e75f311b1201edadc18f12cb
NewSkyLine-dev <fabian.oppermann@gmx.net>
2024-12-30 11:38:02 +0100

    30-12-2024

--- POS/C#/WPF Indicator/IndicatorTemplate/Indicator.cs
+++ POS/C#/WPF Indicator/IndicatorTemplate/Indicator.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.Diagnostics.Metrics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -13,131 +14,96 @@ namespace IndicatorTemplate
             DefaultStyleKeyProperty.OverrideMetadata(typeof(Indicator), new FrameworkPropertyMetadata(typeof(Indicator)));
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        #region Dependency Properties
 
-        public static readonly DependencyProperty MinValueProperty =
-            DependencyProperty.Register(
-                "MinValue",
-                typeof(int),
-                typeof(Indicator),
-                new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, UpdateIndicator));
-
-        public static readonly DependencyProperty MaxValueProperty =
-            DependencyProperty.Register(
-                "MaxValue",
-                typeof(int),
-                typeof(Indicator),
-                new FrameworkPropertyMetadata(287, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, UpdateIndicator));
-
-        public static readonly DependencyProperty CurrentValueProperty =
-            DependencyProperty.Register(
-                "CurrentValue",
-                typeof(int),
-                typeof(Indicator),
-                new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, UpdateIndicator));
-
-
-        public int ImageSize
+        public double Minimum
         {
-            get { return (int)GetValue(ImageSizeProperty); }
-            set { SetValue(ImageSizeProperty, value); }
+            get { return (double)GetValue(MinimumProperty); }
+            set { SetValue(MinimumProperty, value); }
         }
 
-        public static readonly DependencyProperty ImageSizeProperty =
-            DependencyProperty.Register(
-                "ImageSize",
-                typeof(int),
-                typeof(Indicator),
-                new FrameworkPropertyMetadata(100, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, UpdateImageSize));
+        public static readonly DependencyProperty MinimumProperty =
+            DependencyProperty.Register("Minimum",
+                                        typeof(double),
+                                        typeof(Indicator),
+                                        new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.AffectsRender, new PropertyChangedCallback(OnMinimumMaximumValueChanged)));
 
-        private static void UpdateImageSize(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        public double Maximum
+        {
+            get { return (double)GetValue(MaximumProperty); }
+            set { SetValue(MaximumProperty, value); }
+        }
+
+        public static readonly DependencyProperty MaximumProperty =
+            DependencyProperty.Register("Maximum",
+                                        typeof(double),
+                                        typeof(Indicator),
+                                        new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.AffectsRender, new PropertyChangedCallback(OnMinimumMaximumValueChanged)));
+
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value",
+                                        typeof(double),
+                                        typeof(Indicator),
+                                        new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.AffectsRender, new PropertyChangedCallback(OnMinimumMaximumValueChanged)));
+
+        public double PointerAngle
+        {
+            get { return (double)GetValue(PointerAngleProperty); }
+            private set { SetValue(PointerAngleProperty, value); }
+        }
+
+        public static readonly DependencyProperty PointerAngleProperty =
+            DependencyProperty.Register("PointerAngle", 
+                                        typeof(double), 
+                                        typeof(Indicator),
+                                        new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.AffectsRender));
+
+
+        public double ViewSize
+        {
+            get { return (double)GetValue(ViewSizeProperty); }
+            set { SetValue(ViewSizeProperty, value); }
+        }
+
+        public static readonly DependencyProperty ViewSizeProperty =
+            DependencyProperty.Register("ViewSize",
+                                        typeof(double),
+                                        typeof(Indicator),
+                                        new FrameworkPropertyMetadata(100.0, FrameworkPropertyMetadataOptions.AffectsRender));
+
+
+        #endregion
+
+        private static void OnMinimumMaximumValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             Indicator indicator = (Indicator)d;
-            Grid imageGrid = (Grid)indicator.GetTemplateChild("PART_IMAGEPANEL");
-
-            ScaleTransform scale = new()
-            {
-                ScaleX = indicator.ImageSize,
-                ScaleY = indicator.ImageSize
-            };
-
-            imageGrid.RenderTransform = scale;
-            imageGrid.RenderTransformOrigin = new Point(0.5, 0.5);
+            indicator?.UpdatePointerAngle();
         }
 
-        private static void UpdateIndicator(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        private void UpdatePointerAngle()
         {
-            if (d is Indicator indicator)
-            {
-                int minValue = indicator.MinValue;
-                int maxValue = indicator.MaxValue;
-                int currentValue = indicator.CurrentValue;
-
-                Grid imageGrid = (Grid)indicator.GetTemplateChild("PART_IMAGEPANEL");
-                Image image = (Image)imageGrid.Children[1];
-
-                double angle = Math.Clamp(currentValue, minValue, maxValue);
-
-                RotateTransform rotateTransform = new(angle)
-                {
-                    CenterX = 0.5,
-                    CenterY = 0.5
-                };
-
-                image.RenderTransform = rotateTransform;
-                image.RenderTransformOrigin = new Point(0.5, 0.5);
-            }
-        }
-
-        public int MinValue
-        {
-            get => (int)GetValue(MinValueProperty);
-            set
-            {
-                SetValue(MinValueProperty, value);
-            }
-        }
-
-        public int MaxValue
-        {
-            get => (int)GetValue(MaxValueProperty);
-            set
-            {
-                SetValue(MaxValueProperty, value);
-            }
-        }
-
-        public int CurrentValue
-        {
-            get => (int)GetValue(CurrentValueProperty);
-            set
-            {
-                SetValue(CurrentValueProperty, value);
-            }
+            PointerAngle = Value;
+            Console.WriteLine(Value);
         }
 
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
 
-            Grid imageGrid = (Grid)GetTemplateChild("PART_IMAGEPANEL");
+            Image meterBackground = (Image)Template.FindName("PART_METERBACKGROUND", this);
+            Image meterIndicator = (Image)Template.FindName("PART_METERINDICATOR", this);
 
-            Image indicator = new()
+            if (meterBackground != null && meterIndicator != null)
             {
-                Source = new BitmapImage(new Uri("pack://application:,,,/IndicatorTemplate;component/resources/indicator.png")),
-                Stretch = Stretch.Fill,
-                Width = 100
-            };
-
-            Image rounded = new()
-            {
-                Source = new BitmapImage(new Uri("pack://application:,,,/IndicatorTemplate;component/resources/rounded.png")),
-                Stretch = Stretch.Fill,
-                Width = 100
-            };
-
-            imageGrid.Children.Add(rounded);
-            imageGrid.Children.Add(indicator);
+                meterBackground.Source = new BitmapImage(new Uri("pack://application:,,,/IndicatorTemplate;component/Resources/MeterBackground.png"));
+                meterIndicator.Source = new BitmapImage(new Uri("pack://application:,,,/IndicatorTemplate;component/Resources/MeterPointer.png"));
+            }
         }
     }
 }
